6d281abfdc8761dce939ed2491279aa923e9aed4
leiyuyurunrun <116471071+leiyuyurunrun@users.noreply.github.com>
2023-02-27 21:08:51 +0800

    Update paydirect.js

--- paydirect.js
+++ paydirect.js
@@ -6,7 +6,7 @@ launchApp("支付宝");
 sleep(3000);
 toast("我来讲个更浪漫的故事：")
 
-var baba_nongchang = text("芭芭农场").findOne(2000);
+var baba_nongchang =className("android.widget.RelativeLayout").depth(18).indexInParent(12).findOne(2000);
 var click_true = click(baba_nongchang.bounds().centerX(), baba_nongchang.bounds().centerY());
 sleep(5000);
 
