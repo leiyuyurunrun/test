954540d286522d14ec3ef40301a6a5f08406a7d2
leiyuyurunrun <116471071+leiyuyurunrun@users.noreply.github.com>
2023-02-27 11:37:35 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+auto.waitFor();
+setScreenMetrics(1176, 2400);
+sleep(6000);
+
+
+var baba_nongchang = text('芭芭农场').findOne(2000);
+var click_true = click(baba_nongchang.bounds().centerX(), baba_nongchang.bounds().centerY());
+sleep(3000);
+
+click(80, 500);
+var i = 0;
+while(i<200){
+  i++;
+	click(500, 1900);
+  sleep(1000);
+}
+console.show()
+log("完成");
