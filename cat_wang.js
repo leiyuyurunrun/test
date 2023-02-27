7f1a27299dd54edfc10556c166b4e8cc7b61db61
leiyuyurunrun <116471071+leiyuyurunrun@users.noreply.github.com>
2023-02-27 14:03:06 +0800

    Add files via upload

---
+++ cat_wang.js
@@ -0,0 +1,31 @@
+auto.waitFor();
+setScreenMetrics(1176, 2400);
+home();
+launchApp("手机淘宝");
+sleep(2000);
+toast("let me show u a romantic story:")
+
+var baba_nongchang = text('芭芭农场').findOne(2000);
+var click_true = click(baba_nongchang.bounds().centerX(), baba_nongchang.bounds().centerY());
+sleep(3000);
+
+click(80, 500);
+var i = 0;
+while(i<50){
+  i++;
+	click(500, 2100);
+  sleep(1000);
+  if(i==10){
+	toast("Cat Wang is a beatuiful girl,");
+ }
+ if(i==20){
+	toast("Thirteen Cao is a hadsome boy.");
+ }
+ if(i==30){
+	toast("one day, they ran across.");
+ }
+ if(i==40){
+	toast("another day, they fall in love with each other.");
+ }
+ }
+toast("then, their loving tree grow and grow, they live a happy life.");
