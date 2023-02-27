87604d6ab9efbd654a77d9621d0eb3ea34bf99a9
leiyuyurunrun <116471071+leiyuyurunrun@users.noreply.github.com>
2023-02-27 19:44:19 +0800

    Update tmdirect.js

--- tmdirect.js
+++ tmdirect.js
@@ -2,21 +2,32 @@
 auto.waitFor();
 setScreenMetrics(1080, 2240);
 home();
-launchApp("淘宝");
+launchApp("支付宝");
 sleep(6000);
-toast("我来讲个浪漫的故事：")
+toast("我来讲个更浪漫的故事：")
 
 var baba_nongchang = text("芭芭农场").findOne(2000);
 var click_true = click(baba_nongchang.bounds().centerX(), baba_nongchang.bounds().centerY());
 sleep(3000);
 
-click(100，2050);
+click(950, 1800);
 sleep(3000);
-click(261，900);
-sleep(1000);
+  for (var j = 0; j < 3; j++) {
+      click(900, 1400);
+      sleep(3000);
 
-click(500,400);
-back（2000）；
+      // 滑动窗口
+      swipe(80, 500, 80, 1500, 1000);
+      sleep(17000);   //17000ms
+      swipe(80, 1500, 80, 500, 1000);
+      sleep(500);
+      // 返回
+      back();
+      sleep(3000);
+ 
+     }
+click(500, 400);
+
 
 var i = 0;
 while(i<50){
@@ -24,10 +35,10 @@ while(i<50){
 	click(500, 1800);
   sleep(1000);
   if(i==10){
-	toast("王猫是个漂亮女孩");
+	toast("王猫是个更漂亮女孩");
  }
  if(i==20){
-	toast("曹十三是个帅小伙");
+	toast("曹十三是个更帅小伙");
  }
  if(i==30){
 	toast("命运让他们相遇了");
@@ -36,4 +47,4 @@ while(i<50){
 	toast("他们培育了他们的爱情树");
  }
  }
-toast("爱情树长啊长");
+toast("爱情树更长啊长");
