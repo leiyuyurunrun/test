1ff009adddeeda79a9296c59cb17f68d6b50a0d4
leiyuyurunrun <116471071+leiyuyurunrun@users.noreply.github.com>
2023-03-02 09:49:21 +0800

    Update paydirect.js

--- paydirect.js
+++ paydirect.js
@@ -4,7 +4,6 @@ setScreenMetrics(1080, 2240);
 home();
 launchApp("支付宝");
 sleep(3000);
-toast("我来讲个更浪漫的故事：")
 
 var baba_nongchang = text('芭芭农场').findOne(2000);
 var click_true = click(baba_nongchang.bounds().centerX(), baba_nongchang.bounds().centerY());
@@ -43,10 +42,10 @@ click(500, 400);
 
 
 var i = 0;
-while(i<50){
+while(i<100){
   i++;
   click(500, 1800);
   sleep(1000);
  
  }
-toast("爱情树更长啊长");
+
