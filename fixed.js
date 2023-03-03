9615d89b0435b2da020349b2c0fae25943a73a88
leiyuyurunrun <116471071+leiyuyurunrun@users.noreply.github.com>
2023-03-03 19:51:09 +0800

    Create fixed.js

---
+++ fixed.js
@@ -0,0 +1,16 @@
+
+setScreenMetrics(1080, 2240);
+sleep(4000);
+
+var farm = text('芭芭农场').findOne();
+var click_true = click(farm.bounds().centerX(), farm.bounds().centerY());
+sleep(5000);
+
+var i = 0;
+while(i<150){
+  i++;
+
+  click(500, 1850);
+  sleep(1000);
+ }
+
